9200ba078722001b0a697c8d11c7335c37f1b77a
Peter Sorensen <peter.sorensen@get10up.com>
2017-07-14 12:12:09 -0700

    init commit

---
+++ assets/js/content-checker.js
@@ -0,0 +1,54 @@
+/**
+ * General theme functionality
+ */
+
+( function( $ ) {
+
+	'use strict';
+
+	$('#publish').on('click', function(event) {
+		event.preventDefault();
+		var $images = $('#content_ifr').contents().find('img');
+		var insecure = 0;
+		$images.each(function (index, el) {
+			var $this = $(this);
+
+			$this.css('border', '');
+
+			if (el.src.startsWith('http://')) {
+				$this.css('border', '1px solid red');
+				insecure += 1;
+			}
+		});
+		if (insecure > 0) {
+			var $this = $(this);
+			$('.wrap hr').next().remove();
+			var image = insecure > 1 ? 'images' : 'image';
+			var $div = $(
+				'<div>',
+				{
+					'class' : 'error',
+					'html' :  insecure +  ' insecure ' + image + ' found. Please update to https or save image to the media library and insert again.<br>' +
+					'More information'
+				}
+			);
+			var html = '<ol>' +
+							'<li><a target="_blank" href="https://en.support.wordpress.com/add-media/">How to add media</a></li>' +
+							'<li>' +
+								'<a target="_blank" href="https://developers.google.com/web/fundamentals/security/prevent-mixed-content/what-is-mixed-content">Mixed Content</a>' +
+							'</li>' +
+						'</ol>';
+			$div.css({
+				'padding' : '16px',
+				'margin-top' : '16px',
+			});
+			$(html).appendTo($div);
+			$('.wrap hr').after($div);
+			$('body').animate({scrollTop: $div.offset().top - $('#wpadminbar').height()}, 500);
+		} else {
+			$('#post').submit();
+		}
+	});
+
+
+} ) ( jQuery );
---
+++ ipm-insecure-content.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Insecure Content Warning
+Description: Add warning for insecure content when posting
+Author: 10up
+Author URI: http://10up.com/
+Version: 1.0.0
+Text Domain: insecure-content-warning
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+namespace TenUp\InsecureContent;
+
+define( 'INSECURE_CONTENT_URL', plugin_dir_url( __FILE__ ) );
+define( 'INSECURE_CONTENT_TEMPLATE_URL', get_template_directory_uri() );
+define( 'INSECURE_CONTENT_PATH', __DIR__ . DIRECTORY_SEPARATOR );
+define( 'INSECURE_CONTENT_INC', INSECURE_CONTENT_PATH . 'includes' . DIRECTORY_SEPARATOR );
+define( 'INSECURE_CONTENT_VERSION', '1.0.0' );
+
+add_action( 'admin_enqueue_scripts', function () {
+	wp_enqueue_script(
+		'insecure-content-admin',
+		INSECURE_CONTENT_URL . "/assets/js/content-checker.js",
+		[],
+		INSECURE_CONTENT_VERSION,
+		true
+	);
+} );
