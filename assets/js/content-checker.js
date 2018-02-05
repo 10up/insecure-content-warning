c3b48b37e7313891af6bd5dc415cba0b8789bda6
David Green <coding@davidrg.com>
2018-02-04 23:21:32 -0600

    Show list of insecure image URLs

--- assets/js/content-checker.js
+++ assets/js/content-checker.js
@@ -9,6 +9,7 @@
 	$('#publish').on('click', function(event) {
 		var insecure = 0;
 		var $images;
+		var insecureImageURLs = [];
 		if ( $('#wp-content-wrap').hasClass('tinymce-active') ) {
 			$images = $('#content_ifr').contents().find('img');
 		} else {
@@ -17,22 +18,28 @@
 		$images.each( function( index, el ) {
 			if ( el.src && 'https://' !== el.src.substr( 0, 8 ) ) {
 				insecure += 1;
+				insecureImageURLs.push( el.src );
 			}
 		} );
 		if (insecure > 0) {
 			event.preventDefault();
 			var $this = $(this);
 			$('.wrap hr').next().remove();
+			var html;
 			var image = insecure > 1 ? 'images' : 'image';
 			var $div = $(
 				'<div>',
 				{
 					'class' : 'error',
-					'html' :  insecure +  ' insecure ' + image + ' found. Please update image paths to https or save image to the media library and insert again.<br>' +
-					'More information'
+					'html' :  insecure +  ' insecure ' + image + ' found. Please update image paths to https or save image to the media library and insert again.'
 				}
 			);
-			var html = '<ol>' +
+			html = '<ol>';
+			for ( var i = 0, length = insecureImageURLs.length; i < length; i++ ) {
+				html += '<li>' + insecureImageURLs[ i ] + '</li>';
+			}
+			html += '</ol>';
+			html += '<p><strong>More Information:</strong></p><ol>' +
 							'<li><a target="_blank" href="https://en.support.wordpress.com/add-media/">How to add media</a></li>' +
 							'<li>' +
 								'<a target="_blank" href="https://developers.google.com/web/fundamentals/security/prevent-mixed-content/what-is-mixed-content">Mixed Content</a>' +
