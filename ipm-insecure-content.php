e8f6d18b0f51e36c8e282dfcfdc1ae32ab2e75f7
David Green <coding@davidrg.com>
2018-02-15 09:30:06 -0600

    Increment version number

--- ipm-insecure-content.php
+++ ipm-insecure-content.php
@@ -15,7 +15,7 @@ define( 'INSECURE_CONTENT_URL', plugin_dir_url( __FILE__ ) );
 define( 'INSECURE_CONTENT_TEMPLATE_URL', get_template_directory_uri() );
 define( 'INSECURE_CONTENT_PATH', __DIR__ . DIRECTORY_SEPARATOR );
 define( 'INSECURE_CONTENT_INC', INSECURE_CONTENT_PATH . 'includes' . DIRECTORY_SEPARATOR );
-define( 'INSECURE_CONTENT_VERSION', '1.0.0' );
+define( 'INSECURE_CONTENT_VERSION', '1.0.1' );
 
 add_action( 'admin_enqueue_scripts', function () {
 	wp_enqueue_script(
