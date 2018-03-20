c0c3edea80645b21ac54cda28edcbab11596fce8
Peter Sorensen <peter.sorensen@get10up.com>
2018-03-20 11:29:35 -0700

    webpack and es6

--- ipm-insecure-content.php
+++ ipm-insecure-content.php
@@ -15,12 +15,12 @@ define( 'INSECURE_CONTENT_URL', plugin_dir_url( __FILE__ ) );
 define( 'INSECURE_CONTENT_TEMPLATE_URL', get_template_directory_uri() );
 define( 'INSECURE_CONTENT_PATH', __DIR__ . DIRECTORY_SEPARATOR );
 define( 'INSECURE_CONTENT_INC', INSECURE_CONTENT_PATH . 'includes' . DIRECTORY_SEPARATOR );
-define( 'INSECURE_CONTENT_VERSION', '1.0.1' );
+define( 'INSECURE_CONTENT_VERSION', '1.1.0' );
 
 add_action( 'admin_enqueue_scripts', function () {
 	wp_enqueue_script(
 		'insecure-content-admin',
-		INSECURE_CONTENT_URL . "/assets/js/content-checker.js",
+		INSECURE_CONTENT_URL . 'dist/main.js',
 		[],
 		INSECURE_CONTENT_VERSION,
 		true
