69fa95d128a34eff13148d5dbadec9483850c6fa
William Patton <will@pattonwebz.com>
2018-06-22 21:55:18 +0100

    require_once is a statement - no need for parentheses

--- insecure-content-warning.php
+++ insecure-content-warning.php
@@ -20,8 +20,8 @@ define( 'INSECURE_CONTENT_PATH', __DIR__ . DIRECTORY_SEPARATOR );
 define( 'INSECURE_CONTENT_INC', INSECURE_CONTENT_PATH . 'inc' . DIRECTORY_SEPARATOR );
 define( 'INSECURE_CONTENT_VERSION', '1.0' );
 
-require_once( INSECURE_CONTENT_INC . 'assets.php' );
-require_once( INSECURE_CONTENT_INC . 'rest.php' );
+require_once INSECURE_CONTENT_INC . 'assets.php';
+require_once INSECURE_CONTENT_INC . 'rest.php';
 
 Assets\setup();
 Rest\setup();
