7fc5501f79419678a1727f685203b618c127a774
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2021-08-16 16:36:44 +0530

    fix: check featured image

---
+++ src/js/utils/gutenberg-scan-featured-image.js
@@ -0,0 +1,51 @@
+const { select } = wp.data;
+
+/**
+ * Returns all insecure URLs from ghe featured image.
+ *
+ * @param {number} id Attachmet ID.
+ * @return Array
+ */
+export function gutenberg_scan_featured_images(id = 0) {
+	if (id === 0) {
+		return [];
+	}
+
+	const { getMedia } = select('core');
+
+	// Return the media object by attachment ID.
+	const featuredImageObj = getMedia(id);
+
+	/**
+	 * Array to hold source URLs of all sizes
+	 * for a given attachment.
+	 */
+	const sourceUrls = [];
+
+	// The main source URL.
+	if (featuredImageObj.source_url) {
+		sourceUrls.push(featuredImageObj.source_url);
+	}
+
+	/**
+	 * Iterates over source URL for each image size
+	 * and adds it to the `sourceUrls` array.
+	 */
+	if (featuredImageObj.media_details) {
+		Object.keys(featuredImageObj.media_details.sizes).forEach((item) => {
+			const sourceUrl = featuredImageObj.media_details.sizes[item].source_url;
+			if (sourceUrl) {
+				sourceUrls.push(sourceUrl);
+			}
+		});
+	}
+
+	/**
+	 * Filters all the `http://` URLs from the `sourceUrls`.
+	 */
+	const insecureUrls = sourceUrls.filter((item) => {
+		return item.indexOf('https://') === -1;
+	});
+
+	return insecureUrls;
+}
--- src/js/utils/gutenberg-scan.js
+++ src/js/utils/gutenberg-scan.js
@@ -1,4 +1,5 @@
 import { scanElements } from './scan-elements';
+import { gutenberg_scan_featured_images } from './gutenberg-scan-featured-image';
 
 const { select } = wp.data;
 
@@ -6,6 +7,22 @@ export const gutenbergScan = () => {
 	const content = select('core/editor').getEditedPostAttribute('content');
 	const $elements = jQuery.parseHTML(content);
 	const scanResults = scanElements(jQuery($elements).find('*').toArray());
+	const featuredMediaId = select('core/editor').getEditedPostAttribute('featured_media');
+	const featuredImageInsecureUrls = gutenberg_scan_featured_images(featuredMediaId);
+
+	/**
+	 * Calculate number of insecure URLs.
+	 */
+	scanResults.insecure += featuredImageInsecureUrls.length;
+
+	/**
+	 * Merge insecure URLs from different parts of the
+	 * edit page.
+	 */
+	scanResults.insecureElementURLs = [
+		...scanResults.insecureElementURLs,
+		...featuredImageInsecureUrls,
+	];
 
 	return scanResults;
 };
