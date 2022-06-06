0e06d0c535d96cef4dffe7610f95b6608ba14642
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-06-06 10:32:43 +0300

    Classic Create Post command

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -35,7 +35,9 @@ Cypress.Commands.add("editBlockAsHTML", (id) => {
 	cy.get(
 		'.block-editor-block-contextual-toolbar[aria-label="Block tools"]'
 	).within(() => {
-		cy.get('.components-button[aria-label="Options"], .components-button[aria-label="More options"]').click();
+		cy.get(
+			'.components-button[aria-label="Options"], .components-button[aria-label="More options"]'
+		).click();
 	});
 	cy.get(".components-button").contains("Edit as HTML").click();
 });
@@ -64,3 +66,43 @@ Cypress.Commands.add("insertInsecureBlock", (after) => {
 		}
 	});
 });
+
+Cypress.Commands.add(
+	"classicCreatePost",
+	({
+		postType = "post",
+		title = "Test Post",
+		content = "Test content",
+		status = "publish",
+		beforeSave,
+	}) => {
+		cy.visit(`/wp-admin/post-new.php?post_type=${postType}`);
+
+		cy.get("#title").click().clear().type(title);
+
+		cy.get("#content_ifr").then(($iframe) => {
+			const doc = $iframe.contents().find("body#tinymce");
+			cy.wrap(doc).find("p:last-child").type(content);
+		});
+
+		if ("undefined" !== typeof beforeSave) {
+			beforeSave();
+		}
+
+		cy.intercept("POST", "/wp-admin/post.php", (req) => {
+			req.alias = "savePost";
+		});
+
+		if ("draft" === status) {
+			cy.get("#save-post").click();
+		} else {
+			cy.get("#publish").click();
+		}
+
+		cy.wait("@savePost").then((response) => {
+			const body = new URLSearchParams(response.request?.body);
+			const id = body.get("post_ID");
+			cy.wrap(id);
+		});
+	}
+);
