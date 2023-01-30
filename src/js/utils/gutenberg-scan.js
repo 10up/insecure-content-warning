0f88377c0d8fd6fd57951fdfdbc7dc9abfab1f7d
Nate Conley <nate.conley@get10up.com>
2023-01-30 10:42:25 -1000

    Check parent elements in the block editor

--- src/js/utils/gutenberg-scan.js
+++ src/js/utils/gutenberg-scan.js
@@ -5,7 +5,10 @@ const { select } = wp.data;
 export const gutenbergScan = () => {
 	const content = select('core/editor').getEditedPostAttribute('content');
 	const $elements = jQuery.parseHTML(content);
-	const scanResults = scanElements(jQuery($elements).find('*').toArray());
+	const scanResults = scanElements([
+		...jQuery($elements).toArray(),
+		...jQuery($elements).find('*').toArray(),
+	]);
 
 	return scanResults;
 };
