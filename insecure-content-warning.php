6975365cbafcd2276e24067362178a7dd6b3bd4b
Konstantinos Galanakis <kmgalanakis@gmail.com>
2023-08-14 10:00:02 +0300

    Add a check to ensure we have a valid PHP version before loading plugin functionality and output an admin notice if needed

--- insecure-content-warning.php
+++ insecure-content-warning.php
@@ -24,6 +24,53 @@ define( 'INSECURE_CONTENT_PATH', __DIR__ . DIRECTORY_SEPARATOR );
 define( 'INSECURE_CONTENT_INC', INSECURE_CONTENT_PATH . 'includes' . DIRECTORY_SEPARATOR );
 define( 'INSECURE_CONTENT_VERSION', '1.1.0' );
 
+/**
+ * Get the minimum version of PHP required by this plugin.
+ *
+ * @since 1.1.2
+ *
+ * @return string Minimum version required.
+ */
+function minimum_php_requirement(): string {
+	return '7.4';
+}
+
+/**
+ * Whether PHP installation meets the minimum requirements
+ *
+ * @since 1.1.2
+ *
+ * @return bool True if meets minimum requirements, false otherwise.
+ */
+function site_meets_php_requirements(): bool {
+	return version_compare( phpversion(), minimum_php_requirement(), '>=' );
+}
+
+// Try to load the plugin files, ensuring our PHP version is met first.
+if ( ! site_meets_php_requirements() ) {
+	add_action(
+		'admin_notices',
+		function() {
+			?>
+			<div class="notice notice-error">
+				<p>
+					<?php
+					echo wp_kses_post(
+						sprintf(
+						/* translators: %s: Minimum required PHP version */
+							__( 'Insecure Content Warning requires PHP version %s or later. Please upgrade PHP or disable the plugin.', 'insecure-content-warning' ),
+							esc_html( minimum_php_requirement() )
+						)
+					);
+					?>
+				</p>
+			</div>
+			<?php
+		}
+	);
+	return;
+}
+
 require_once INSECURE_CONTENT_INC . 'assets.php';
 require_once INSECURE_CONTENT_INC . 'rest.php';
 require_once INSECURE_CONTENT_INC . 'admin.php';
